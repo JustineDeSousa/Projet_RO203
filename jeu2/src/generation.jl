07cb9be58d5bd79da4fff457157e70ce55fdeb7c
Naiina <nina.gregorio@free.fr>
2021-04-05 15:08:37 +0200

    petit ajout a generinstances

--- jeu2/src/generation.jl
+++ jeu2/src/generation.jl
@@ -38,6 +38,13 @@ function generateInstance(n::Int64)
 			end
 		end
 	end 
+	for i=1:n
+		for j=1:n
+			if x[i,j]==0
+				x[i,j]=ceil.(Int, n * rand())
+			end
+		end
+	end
 	println("x:",x)
 	println("y:",y)
 end 
